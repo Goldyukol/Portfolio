19b3f0a222d8c0bd70f3dea15e3942c1ec762b6d
Yury <goldyukol12@gmail.com>
2019-09-30 23:26:50 +0300

    start: start write my portfolio

---
+++ src/NavMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './NavMenu.module.css';
+
+
+function NavMenu() {
+  return (
+    <div className={s.navMenu}>
+      <a href='#' className={s.link}>Главная</a>
+      <a href='#' className={s.link}>Скиллы</a>
+      <a href='#' className={s.link}>Проекты</a>
+      <a href='#' className={s.link}>Контакты</a>
+    </div>
+  );
+}
+
+export default NavMenu;
