bdc63c886fa0d00a78cf99af42f080500b72ee30
Yury <goldyukol12@gmail.com>
2019-10-07 23:12:54 +0300

    Updates

---
+++ precache-manifest.c0663f1ed2188164dbd233051e17ba61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f8598e046b5eed4289b1760ac5e48d",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "6fc19689a7c304a1da48",
+    "url": "/Portfolio/static/css/main.649a3753.chunk.css"
+  },
+  {
+    "revision": "b98c22c9d0a36737fcd2",
+    "url": "/Portfolio/static/js/2.86eed31f.chunk.js"
+  },
+  {
+    "revision": "6fc19689a7c304a1da48",
+    "url": "/Portfolio/static/js/main.e06c2faa.chunk.js"
+  },
+  {
+    "revision": "6e290e45fa9068bd6272",
+    "url": "/Portfolio/static/js/runtime-main.faed0cb7.js"
+  },
+  {
+    "revision": "88b6a716be2d8391c9822d8a2eee6fd2",
+    "url": "/Portfolio/static/media/1_jA5lTgPRbyimsFNod7SlFQ.88b6a716.png"
+  }
+]);
